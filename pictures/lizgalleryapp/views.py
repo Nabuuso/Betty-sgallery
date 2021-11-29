59d1888901b3e1d448efa59bd1c02ba299724008
betty <betty.nabuuso@moringaschool.com>
2021-11-29 16:22:00 +0300

    opening the application again

--- pictures/lizgalleryapp/views.py
+++ pictures/lizgalleryapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+# from django.template import loader
+import datetime as dt
+# from .models import Image
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def index(request):
+    date = dt.date.today()
+    # images = Image.get_images()
+    return render(request, 'index.html', {"date": date,})
